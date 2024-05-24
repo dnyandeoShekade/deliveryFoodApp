7d6edb87835eae113eb3cbe0e6f449eab0e07f28
shekade dnyandeo <shekadednyande@gmail.com>
2024-05-24 23:04:26 +0530

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+import Header from '../../component/Header/Header'
+import ExploreMenu from '../../component/ExploreMenu.jsx/ExploreMenu'
+import FoodDisplay from '../../component/FoodDisplay/FoodDisplay'
+import AppDownload from '../../component/AppDownload/AppDownload'
+
+const Home = () => {
+  const [category, setCategory] = useState("All");
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+
+      <FoodDisplay category={category}/>
+      <AppDownload/>
+    </div>
+  )
+}
+
+export default Home
